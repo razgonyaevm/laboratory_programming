1b84e50411f39ca275287c49a102839f693872ee
razgonyaevm <90210839+razgonyaevm@users.noreply.github.com>
2024-09-12 07:52:35 +0300

    Update laboratory_one.java

--- laboratory_one.java
+++ laboratory_one.java
@@ -1,1 +1,14 @@
-
+public class Laboratory_one {
+  public static double rand(double min, double max) {
+    max -= min;
+    return (Math.random() * ++max) + min;
+  }
+  public static void main(String[] args) {
+    int[] s;
+    s = new int[11];
+    for (int i = 1; i <= 11; i++) {
+        s[i - 1] = i * 2;
+        System.err.println(s[i - 1]);
+    }
+  }
+}
