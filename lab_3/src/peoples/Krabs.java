36caf8a7bcbae0ac298319d127fb1fc06eb5fc41
razgonyaevm <90210839+razgonyaevm@users.noreply.github.com>
2024-12-18 09:34:36 +0300

    Update Krabs.java

--- lab_3/src/peoples/Krabs.java
+++ lab_3/src/peoples/Krabs.java
@@ -4,15 +4,15 @@ import lab3.abstracts.People;
 import lab3.enums.Classes;
 import lab3.enums.Moods;
 
-public final class SimplePeople extends People {
-    public SimplePeople(String name, Moods moods, Classes classes) {
-        super.setMood(moods);
+public final class Skuperfild extends People {
+    public Skuperfild(Moods mood, Classes classes) {
+        super.setName("Скуперфильд");
+        super.setMood(mood);
         super.setClasses(classes);
-        super.setName(name);
     }
 
     @Override
     public void say(String text) {
-        System.out.println(super.getName() + " сказал: " + text);
+        System.out.println("Скуперфильд сказал: " + text);
     }
 }
